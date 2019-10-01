a0d102e2333cf7dfc04c49d97ed24884ab66693d
Jon Sensenig <jsen@jonmbp.local>
2019-10-01 19:07:00 -0400

    Add data structure for TC and algorithm parameters accessible from top level.

--- src/ModuleTrigger.h
+++ src/ModuleTrigger.h
@@ -3,9 +3,16 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <utility>  
 #include <algorithm>
 #include <cmath>
+#include "AdjacencyAlgorithms.h"
 
-using namespace std;
+int ModuleTrigger(std::vector<TC> candidates, uint32_t size_thresh, std::pair <int,int> window_gaps, std::pair <int,int> APA_gaps, double slope_diff, int nAPAs);
 
-int ModuleTrigger(vector<vector<int>> candidates);
+inline double uint32abs( const uint32_t& lhs, const uint32_t& rhs ) {
+   return lhs>rhs ? (double)(lhs-rhs) : (double)(rhs-lhs);
+}
+inline double uint64abs( const uint64_t& lhs, const uint64_t& rhs ) {
+   return lhs>rhs ? (double)(lhs-rhs) : (double)(rhs-lhs);
+}
